88026d2f27819bcead52339c565951c2b73495ec
Smarika-tech <smarikapokhrel96@gmail.com>
2025-08-07 22:00:42 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>        
+      <h1 className='font-bold bg-red-300'>basic setup</h1>
+    </>
+  )
+}
+
+export default App
