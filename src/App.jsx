a99a43e60a128e20fd985add9d7d45c151000bbc
Smarika-tech <smarikapokhrel96@gmail.com>
2025-08-28 22:08:43 +0545

    state setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const[from,setFrom] =useState("usd")
+  const[to,setTo] =useState("npr")
+  const [convertedAmount, setConvertedAmount]= useState(0)
+  const currencyInfo = useCurrencyInfo(from)
+  
 
   return (
     <>        
